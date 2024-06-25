24ae06d514a7baee871e889688094306c9afde4e
ShrutiJayswal <shrutijayswal2@gmail.com>
2024-06-25 17:10:09 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const id=1234
+let name="shruti"
+var age=19
+city="ahm"
+let account; //undefined.
+
+//id=1  : not allowed .
+
+name ="vipra"
+age=20
+city="bengaluru"
+
+console.table([name, age, city])
+console.log(account);
+
+
+/*
+prefer not to use block
+because of issue in block scope and functional scope. 
+*/
