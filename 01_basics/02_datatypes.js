de448521d13cbae6d3e3db6194a7d10fce0e221e
ShrutiJayswal <shrutijayswal2@gmail.com>
2024-06-27 04:36:49 +0000

    rename datatype file

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treats all JS code as newer version
+let name="shruti"
+let age=19
+let islogged=true
+
+/*
+PRIMITIVE
+number
+bigInt
+string => "" or ''
+boolean => true/1 or false/0
+symbol => unique
+null => standalone value
+undefined => var is declared but value is not assigned.
+
+
+NON-PRIMITIVE
+object
+*/
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object
